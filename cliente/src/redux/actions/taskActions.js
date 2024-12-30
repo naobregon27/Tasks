a4791e6fac55cd72d4ddcb7c8ab6f01c8a804bc2
naobregon27 <na.obregon27@hotmail.com>
2024-12-30 15:53:32 -0300

    final

--- cliente/src/redux/actions/taskActions.js
+++ cliente/src/redux/actions/taskActions.js
@@ -5,22 +5,25 @@ export const CREATE_TASK = 'CREATE_TASK';
 export const UPDATE_TASK = 'UPDATE_TASK';
 export const DELETE_TASK = 'DELETE_TASK';
 
+//const URL = 'http://localhost:3000'
+const URL = "https://tasks-jabl.onrender.com"
+
 export const getTasks = () => async dispatch => {
-    const response = await axios.get('http://localhost:3000/tasks');
+    const response = await axios.get('https://tasks-jabl.onrender.com/tasks');
     dispatch({ type: GET_TASKS, payload: response.data });
 };
 
 export const createTask = task => async dispatch => {
-    const response = await axios.post('http://localhost:3000/tasks', task);
+    const response = await axios.post('https://tasks-jabl.onrender.com/tasks', task);
     dispatch({ type: CREATE_TASK, payload: response.data });
 };
 
 export const updateTask = (id, updates) => async dispatch => {
-    const response = await axios.put(`http://localhost:3000/tasks/${id}`, updates);
+    const response = await axios.put(`https://tasks-jabl.onrender.com/tasks/${id}`, updates);
     dispatch({ type: UPDATE_TASK, payload: response.data });
 };
 
 export const deleteTask = id => async dispatch => {
-    await axios.delete(`http://localhost:3000/tasks/${id}`);
+    await axios.delete(`https://tasks-jabl.onrender.com/tasks/${id}`);
     dispatch({ type: DELETE_TASK, payload: id });
 };
