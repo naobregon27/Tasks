e983afb681348f11aab743f7441b0405e16ba8ca
naobregon27 <na.obregon27@hotmail.com>
2025-01-02 10:54:29 -0300

    B

--- server/src/routers/prueba.router.js
+++ server/src/routers/prueba.router.js
@@ -10,14 +10,14 @@ const {
   deleteTask
 } = require('../controllers/prueba.controller.js');
 
-router.get('/tasks', getTask);
-router.get('/tasks/:id', getTaskById);
-router.post('/tasks', [
+router.get('/api/tasks', getTask);
+router.get('/api/tasks/:id', getTaskById);
+router.post('/api/tasks', [
   check('title').not().isEmpty().withMessage('Title is required'),
 ], postTask);
-router.put('/tasks/:id', [
+router.put('/api/tasks/:id', [
   check('title').optional().not().isEmpty().withMessage('Title is required if provided'),
 ], updateTask);
-router.delete('/tasks/:id', deleteTask);
+router.delete('/api/tasks/:id', deleteTask);
 
 module.exports = router;
