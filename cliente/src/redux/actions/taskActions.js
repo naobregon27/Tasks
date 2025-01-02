0ab127fb4e6e2b55b71a171dc37d2c006776b7fa
naobregon27 <na.obregon27@hotmail.com>
2025-01-02 11:03:11 -0300

    C

--- cliente/src/redux/actions/taskActions.js
+++ cliente/src/redux/actions/taskActions.js
@@ -8,21 +8,21 @@ export const DELETE_TASK = 'DELETE_TASK';
 
 
 export const getTasks = () => async dispatch => {
-    const response = await axios.get('https://tasks-6boo.onrender.com/tasks');
+    const response = await axios.get('https://tasks-6boo.onrender.com/api/tasks');
     dispatch({ type: GET_TASKS, payload: response.data });
 };
 
 export const createTask = task => async dispatch => {
-    const response = await axios.post('https://tasks-6boo.onrender.com/tasks', task);
+    const response = await axios.post('https://tasks-6boo.onrender.com/api/tasks', task);
     dispatch({ type: CREATE_TASK, payload: response.data });
 };
 
 export const updateTask = (id, updates) => async dispatch => {
-    const response = await axios.put(`https://tasks-6boo.onrender.com/tasks/${id}`, updates);
+    const response = await axios.put(`https://tasks-6boo.onrender.com/api/tasks/${id}`, updates);
     dispatch({ type: UPDATE_TASK, payload: response.data });
 };
 
 export const deleteTask = id => async dispatch => {
-    await axios.delete(`https://tasks-6boo.onrender.com/tasks/${id}`);
+    await axios.delete(`https://tasks-6boo.onrender.com/api/tasks/${id}`);
     dispatch({ type: DELETE_TASK, payload: id });
 };
